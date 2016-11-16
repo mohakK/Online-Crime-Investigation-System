f3d9db2a91079807c85c0d914f3153a6fbf071e0
sanjana134 <sg725@snu.edu.in>
2016-11-16 16:40:53 +0530

    Add files via upload

---
+++ messages.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author Sanjana13
+ */
+public class messages {
+    String receiver;
+    String sender;
+    private String key;
+    String encrypt;
+    String decrypt;
+    void send()
+    {
+        //create object and send messages
+    }
+    private void encode()
+    {
+        //to encode the sent messages before they are sent
+    }
+    private void decode()
+    {
+        //to decode the messages received when received
+    }
+    
+}
