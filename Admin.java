a53a51fcaf91308f1a59d7000c4c65a9798c6cec
Mohak Kulshreshtha <mohakk05@gmail.com>
2016-11-16 15:11:07 +0530

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author Mohak
+ */
+public class Admin extends Person {
+    
+    public Admin()
+    {
+        
+    }
+    void addRecord(String Type)
+    {
+        
+    }
+    void deleteRecord(String Type)
+    {
+        
+    }
+    void updateRecord(String Type)
+    {
+        
+    }
+}
