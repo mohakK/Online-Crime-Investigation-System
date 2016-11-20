fac6cca23c44f5965b46a30e5e774f6059327ab4
prabha121 <pm804@snu.edu.in>
2016-11-20 17:05:04 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author prabha121
+ */
+public class User extends Person {
+public User()
+{
+    
+}
+    void viewMessages(String Type)
+    {
+        
+    }
+    void sentMessages(String Type)
+    {
+        
+    }
+    void Location(String Type)
+    {
+        
+    } 
+     void disguise(String Type)
+     {
+
+     }
+      void caseWorking(String Type)
+     {
+
+     }
+      private void encode()
+     {
+           // to encode the messages sent
+     }
+     private void decode()
+     {
+          // to decode the messages received
+     }
+
+    
+}
