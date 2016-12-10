b76a7b56dad0f86b1d5592acf76a30025d458a44
niharika-cnr <nc612@snu.edu.in>
2016-12-11 01:55:22 +0530

    Update aes.java

--- aes.java
+++ aes.java
@@ -7,37 +7,34 @@ import javax.crypto.SecretKey;
 import javax.xml.bind.DatatypeConverter;
 
 /**
-7
  * This example program shows how AES encryption and decryption can be done in Java.
-8
  * Please note that secret key and encrypted text is unreadable binary and hence
-9
  * in the following program we display it in hexadecimal format of the underlying bytes.
-10
  * @author Jayson
-11
  */
 
  class AESEncryption {
   
+  	private static String plainText;
+  	private static SecretKey secKey;
 
-    /**
-15
-     * 1. Generate a plain text for encryption
-16
+  	AESEncryption(String plainText)	{
+  		this.plainText = plainText;
+  	}
+    
+   /**
+     * 1. We have the plain text for encryption
      * 2. Get a secret key (printed in hexadecimal form). In actual use this must
-17
      * by encrypted and kept safe. The same key is required for decryption.
-18
-     * 3.
-19
      */
 
+    public SecretKey getKey()	{return secKey;}
+
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter text to encrypt ");
-        String plainText = sc.next();
-        SecretKey secKey = getSecretEncryptionKey();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter text to encrypt ");
+        // plainText = sc.next();
+        secKey = getSecretEncryptionKey();
         byte[] cipherText = encryptText(plainText, secKey);
         String decryptedText = decryptText(cipherText, secKey);
          
@@ -46,21 +43,13 @@ import javax.xml.bind.DatatypeConverter;
         System.out.println("Encrypted Text (Hex Form):"+bytesToHex(cipherText));
 
         System.out.println("Descrypted Text:"+decryptedText);
-         
-
     }
-
      
     /**
-34
      * gets the AES encryption key. In your actual programs, this should be safely
-35
      * stored.
-36
      * @return
-37
      * @throws Exception
-38
      */
 
     public static SecretKey getSecretEncryptionKey() throws Exception{
@@ -69,25 +58,17 @@ import javax.xml.bind.DatatypeConverter;
 
         generator.init(128); // The AES key size in number of bits
 
-        SecretKey secKey = generator.generateKey();
+        secKey = generator.generateKey();
 
         return secKey;
-    }
-
-     
+    }   
 
     /**
-47
      * Encrypts plainText in AES using the secret key
-48
      * @param plainText
-49
      * @param secKey
-50
      * @return
-51
      * @throws Exception
-52
      */
 
     public static byte[] encryptText(String plainText,SecretKey secKey) throws Exception{
@@ -101,25 +82,15 @@ import javax.xml.bind.DatatypeConverter;
         byte[] byteCipherText = aesCipher.doFinal(plainText.getBytes());
 
         return byteCipherText;
-
-    }
-
-     
+    }   
 
     /**
-62
      * Decrypts encrypted byte array using the key used for encryption.
-63
      * @param byteCipherText
-64
      * @param secKey
-65
      * @return
-66
      * @throws Exception
-67
      */
-
     public static String decryptText(byte[] byteCipherText, SecretKey secKey) throws Exception {
 
         // AES defaults to AES/ECB/PKCS5Padding in Java 7
@@ -134,24 +105,13 @@ import javax.xml.bind.DatatypeConverter;
 
     }
 
-     
-
     /**
-
      * Convert a binary byte array into readable hex form
-78
      * @param hash
-79
      * @return
-80
      */
 
     private static String  bytesToHex(byte[] hash) {
-
        return DatatypeConverter.printHexBinary(hash);
-
     }
-
 }
-
-
